d15c126e8c275022ec690ba0b231f2a922eefe81
julietoge <julietogechi27@gmail.com>
2023-12-14 19:21:37 +0100

    set up app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Hireme from "./components/Hireme";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Bot from "./components/Bot.jsx";
 
 function App() {
-
   return (
-    <div className="App">
-      App
+    <div>
+      <Navbar />
+      <Bot />
+      <Hero />
+      <About />
+      <Skills />
+      <Hireme />
+      <Project />
+      <Contact />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
