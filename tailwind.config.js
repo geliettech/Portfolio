bd91c44640f47dec5997ff07772736349e53a9cd
julietoge <julietogechi27@gmail.com>
2023-12-17 00:27:42 +0100

    updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: "jit",
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: "'Poppins', sans-serif"
+      }
+    },
   },
   plugins: [],
 }
-
