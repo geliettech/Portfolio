ba18d11bb6a020e20a557b93b41211071379c4ae
julietoge <julietogechi27@gmail.com>
2023-12-22 00:10:00 +0100

    updated bot.jsx

--- src/components/Bot.jsx
+++ src/components/Bot.jsx
@@ -1,6 +1,77 @@
-// Bot.js
-import React, { useState } from 'react';
-import Chatbot from './Chatbot';
+import React, { useState } from "react";
+import ChatBot from "react-simple-chatbot";
+import { ThemeProvider } from "styled-components";
+const steps = [
+  {
+    id: 1,
+    message: "Hello! I am OGE, your friendly chatbot.",
+    trigger: 2,
+  },
+  {
+    id: 2,
+    message: "What is your name?",
+    trigger: 3,
+  },
+  {
+    id: 3,
+    user: true,
+    trigger: 4,
+  },
+  {
+    id: 4,
+    message: "Hi {previousValue}, nice to meet you! How can I help you?",
+    trigger: 5,
+  },
+  {
+    id: 5,
+    options: [
+      { value: 1, label: "About OGE", trigger: 6 },
+      { value: 2, label: "OGE services", trigger: 7 },
+      { value: 3, label: "Connect with OGE", trigger: 8 },
+      { value: 4, label: "Contact OGE", trigger: 9 },
+    ],
+  },
+  {
+    id: 6,
+    message:
+      "OGE is a talented frontend Web Developer and a graduate of computer science and informatics from Federal University Otuoke, Nigeria.",
+    trigger: 3,
+  },
+  {
+    id: 7,
+    message:
+      "OGE offers a range of frontend development services, including web design, responsive layouts, and user interface development.",
+    trigger: 3,
+  },
+  {
+    id: 8,
+    message:
+      "You can connect with OGE via LinkedIn: (https://www.linkedin.com/in/Uhegbu-ogechi-juliet). Feel free to reach out for collaborations or inquiries.",
+    trigger: 3,
+  },
+  {
+    id: 9,
+    message:
+      "You can reach out to OGE via email at julietogechi27@gmail.com. Whether you have questions, collaboration ideas, or just want to say hello, OGE would love to hear from you! Additionally, you can follow OGE on Twitter: (https://twitter.com/josephjulietogechi) and GitHub: [OGE's GitHub Profile](https://github.com/julietoge). Stay updated on the latest projects and developments!",
+    trigger: 3,
+  },
+];
+
+const theme = {
+  background: "white",
+  headerBgColor: "#0891b2",
+  headerFontSize: "20px",
+  botBubbleColor: "#0891b2",
+  headerFontColor: "white",
+  botFontColor: "white",
+  userBubbleColor: "#1a202c",
+  userFontColor: "white",
+};
+
+// Set some properties of the bot
+const config = {
+  botAvatar: botAvatarImage,
+};
 
 const Bot = () => {
   const [showChatbot, setShowChatbot] = useState(false);
@@ -20,13 +91,16 @@ const Bot = () => {
 
       {/* Popup Chatbot */}
       {showChatbot && (
-        <div className="fixed bottom-12 right-4 z-[1000] bg-white shadow-md rounded-md p-4 max-w-xs">
-          <Chatbot />
+        <div className="text-gray-900 fixed bottom-5 top-20 sm:right-4 z-[1000] bg-white shadow-md rounded-md">
+          <ThemeProvider theme={theme}>
+            <ChatBot steps={steps} headerTitle="OgeBot" {...config} />
+          </ThemeProvider>
+
           <button
-            className="mt-4 p-2 bg-cyan-600 text-white rounded-md"
+            className="absolute top-0 right-0 z-[1002] p-2 text-2xl text-white rounded-md"
             onClick={toggleChatbot}
           >
-            Close Chat
+            <ion-icon name="close-outline"></ion-icon>
           </button>
         </div>
       )}
