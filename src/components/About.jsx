ebf69d2107ede4ad63cdad8d231c49c29b7ed309
julietoge <julietogechi27@gmail.com>
2023-12-22 10:59:00 +0100

    updated About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import aboutImg from "../assets/about.png";
 
 const About = () => {
-  const info=[
-    {text:'Years Experience', count: '04'},
-    {text:'Completed Projects', count: '18'},
-    {text:'Companies Work', count: '02'},
-  ]
+  const info = [
+    { text: "Years Experience", count: "04" },
+    { text: "Completed Projects", count: "18" },
+    { text: "Companies Work", count: "02" },
+  ];
   return (
     <section id="about" className="py-10 text-white">
       <div className="text-center mt-8">
@@ -14,22 +15,46 @@ const About = () => {
         </h3>
         <p className="text-gray-400 my-3 text-lg">My introduction</p>
         <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
-          <div>
+          <div className="p-2">
             <div className="text-gray-300 my-3">
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae dolor reprehenderit nostrum alias harum odio laborum, unde repellat numquam! Ratione illum expedita quo, assumenda iure similique repellat itaque tempore laudantium.
+              <p className="text-justify leading-7 w-11/12 mx-auto">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae
+                dolor reprehenderit nostrum alias harum odio laborum, unde
+                repellat numquam! Ratione illum expedita quo, assumenda iure
+                similique repellat itaque tempore laudantium.
               </p>
-              <div>
-                {info.map(content=>(
+              <div className="flex mt-10 items-center gap-7">
+                {info.map((content) => (
                   <div key={content.text}>
-                    <h3>{content.count}<span>+</span></h3>
-                    <span>{content.text}</span>
+                    <h3 className="md:text-4x text-2xl font-semibold text-white">
+                      {content.count}
+                      <span className="text-cyan-600">+</span>
+                    </h3>
+                    <span className="md:text-base text-xs">{content.text}</span>
                   </div>
                 ))}
               </div>
+              <br />
+              <br />
+              <a
+                href="https://drive.google.com/drive/mobile/folders/1LvDwWtRQ1Txc1qZYmfjZquioGoqs0aNq"
+                target="_blank"
+                download
+                rel="noopener noreferrer"
+              >
+                <button className="btn-primary">Download CV</button>
+              </a>
             </div>
           </div>
-          <div></div>
+          <div className="flex-1 md:mt-0 mt-6 flex justify-center items-center">
+            <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm aboutImg">
+              <img
+                src={aboutImg}
+                alt=""
+                className="w-full object-cover bg-cyan-600 rounded-xl"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </section>
