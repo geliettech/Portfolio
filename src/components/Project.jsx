3bd70a299d17958b42cc3c558abc254527bcaa25
julietoge <julietogechi27@gmail.com>
2023-12-22 13:06:24 +0100

    updated HireMe.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Project = () => {
   return (
-    <div>Project</div>
-  )
-}
+    <section id="projects" className="py-10 text-white">
+      <div className="text-center">
+        <h3 className="text-4xl font-semibold">
+          My <span className="text-cyan-600">Projects</span>
+        </h3>
+        <p className="text-gray-400 mt-3 text-lg">My awesome works</p>
+      </div>
+      <br />
+      <div className="lex max-w-6xl gap-6 px-5 mx-auto items-center relative">
+        <div className="lg:w-2/3 w-full">
+          
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Project
+export default Project;
