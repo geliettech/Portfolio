77f2cc10ba4932b09bd4336771e197c153e24245
julietoge <julietogechi27@gmail.com>
2023-12-22 01:14:29 +0100

    updated Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import hero from "../assets/hero.png";
 
 const Hero = () => {
-  return (
-    <section className='h-screen'>Hero</section>
-  )
-}
+  const social_media = [
+    "logo-instagram",
+    "logo-facebook",
+    "logo-linkedin",
+    "logo-twitter",
+  ];
 
-export default Hero                                                                                    
+  return (
+    <section
+      id="home"
+      className="min-h-screen flex py-10 md:flex-row flex-col items-center"
+    >
+      <div className="flex-1 flex items-center justify-center h-full">
+        <img src={hero} alt="" className="md:w-11/12 h-full object-cover" />
+      </div>
+      <div className="flex-1">
+        <div className="md:text-left text-center">
+          <h1
+            className="md:text-5xl text-2xl md:leading-normal leading-10
+         text-white font-bold"
+          >
+            <span className="text-cyan-600 md:text-6xl text-5xl">
+              Hello!
+              <br />
+            </span>
+            My name is <span>Uhegbu, Ogechi Juliet</span>
+          </h1>
+          <h4 className="md:text-2xl text-lg md:leading-normal leading-10 mt-4 font-bold text-gray-600">
+            Frontend Developer
+          </h4>
+          <button className="btn-primary mt-8">Contact Me</button>
+          <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5">
+            {social_media?.map((icon) => (
+              <div
+                key={icon}
+                className="text-gray-600 hover:text-white cursor-pointer"
+              >
+                <ion-icon name={icon}></ion-icon>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
